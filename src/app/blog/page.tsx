cbbaf002e2685eab06245dc3a2363251f8fec6bb
meseguraaa <brunoleal2005@yahoo.com.br>
2025-10-30 09:52:15 -0300

    add seo em blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,31 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog - SITE.SET",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "SITE.SET",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://site-blog-khaki-chi.vercel.app/og-image.jpg",
+    siteName: "SITE.SET",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-khaki-chi.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Blog - SITE.SET",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
